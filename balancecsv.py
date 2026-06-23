499a70a6657904e68245b44723b36e67cb8e0fce
Juniper Pasternak <juniper.pasternak@gmail.com>
2026-06-23 16:17:22 -0400

    Add headers to balanced CSVs

--- balancecsv.py
+++ balancecsv.py
@@ -1,8 +1,12 @@
 import pandas as pd
-write_file = open("stateData_2000_10_test.csv", "w")
-data_file = open("stateData_100000_10_test.csv")
+write_file = open("stateData_2000_10.csv", "w")
+data_file = open("stateData_100000_10.csv")
 df = pd.read_csv(data_file)
 switch = True
+
+df_new = pd.DataFrame(columns=df.columns)
+df_new.to_csv(write_file, mode='a', index=False, header=True)
+
 for i in range(len(df)):
     value = df.at[i, "converges"]
     if value % 2 == switch:
