aaa6bd9787f10e510d19cc8967e6ff3347070449
Juniper Pasternak <juniper.pasternak@gmail.com>
2026-06-23 15:54:52 -0400

    Add test dataset

--- balancecsv.py
+++ balancecsv.py
@@ -1,14 +1,13 @@
 import pandas as pd
-file = open("stateData_2000_10.csv", "w")
-data = open("stateData_100000_10.csv")
-df = pd.read_csv(data)
+write_file = open("stateData_2000_10_test.csv", "w")
+data_file = open("stateData_100000_10_test.csv")
+df = pd.read_csv(data_file)
 switch = True
 for i in range(len(df)):
     value = df.at[i, "converges"]
     if value % 2 == switch:
         df_new = pd.DataFrame(df.iloc[[i]])
-        df_new.to_csv(file, mode='a', index=False, header=False)
+        df_new.to_csv(write_file, mode='a', index=False, header=False)
         switch = not switch
-file.close()
-data.close()
-    
+write_file.close()
+data_file.close()
